3f5173e3cc2b9fad79e33b96f69f5bbb140f3271
Raphaël Jakse <raphael.jakse@xwiki.com>
2023-11-07 15:47:00 +0100

    CONFLUENCE-150: Space parameters of the excerpt-include and content-report-table macros are not validated

---
+++ confluence-xml/src/main/java/org/xwiki/contrib/confluence/filter/internal/macros/ExcerptIncludeMacroConverter.java
@@ -0,0 +1,55 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.contrib.confluence.filter.internal.macros;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.contrib.confluence.filter.internal.input.ConfluenceConverter;
+import org.xwiki.model.EntityType;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Convert Confluence include macro to XWiki display macro.
+ *
+ * @version $Id$
+ * @since 9.26.0
+ */
+@Component
+@Singleton
+@Named("excerpt-include")
+public class ExcerptIncludeMacroConverter extends AbstractMacroConverter
+{
+    @Inject
+    private ConfluenceConverter confluenceConverter;
+
+    @Override
+    protected Map<String, String> toXWikiParameters(String confluenceId, Map<String, String> confluenceParameters,
+        String content)
+    {
+        Map<String, String> parameters = new LinkedHashMap<>(1);
+        String spaceRef = confluenceParameters.get("");
+        parameters.put("0", confluenceConverter.convert(spaceRef, EntityType.SPACE));
+        return parameters;
+    }
+}
