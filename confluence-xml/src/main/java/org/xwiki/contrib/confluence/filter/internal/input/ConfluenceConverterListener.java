9244a9de8e4bc3a8d955db6748957a5e104bf13f
Teo Caras <45433221+trrenty@users.noreply.github.com>
2024-02-01 11:36:12 +0200

    [Misc] Fix failing build by removing unused imports

--- confluence-xml/src/main/java/org/xwiki/contrib/confluence/filter/internal/input/ConfluenceConverterListener.java
+++ confluence-xml/src/main/java/org/xwiki/contrib/confluence/filter/internal/input/ConfluenceConverterListener.java
@@ -27,7 +27,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
