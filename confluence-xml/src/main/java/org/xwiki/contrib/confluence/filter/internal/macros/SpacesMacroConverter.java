02624393ed25dac3ba918699138c97e7d82e1e72
farcasut <farcasialexandrububu@gmail.com>
2024-09-02 11:30:02 +0300

    CONFLUENCE-267: Convert spaces macro to documentTree * Added a converter for the spaces macro and modified the old test to include the results of the new converter.

---
+++ confluence-xml/src/main/java/org/xwiki/contrib/confluence/filter/internal/macros/SpacesMacroConverter.java
@@ -0,0 +1,57 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.contrib.confluence.filter.internal.macros;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.rendering.listener.Listener;
+
+/**
+ * Convert the spaces macro.
+ *
+ * @version $Id$
+ * @since 1.51.1
+ */
+@Component
+@Singleton
+@Named("spaces")
+public class SpacesMacroConverter extends AbstractMacroConverter
+{
+    @Override
+    public void toXWiki(String confluenceId, Map<String, String> confluenceParameters, String confluenceContent,
+        boolean inline, Listener listener)
+    {
+        super.toXWiki("documentTree", confluenceParameters, confluenceContent, inline, listener);
+    }
+
+    @Override
+    protected Map<String, String> toXWikiParameters(String confluenceId, Map<String, String> confluenceParameters,
+        String content)
+    {
+        Map<String, String> xwikiParameters = new HashMap<>();
+        xwikiParameters.put("finder", "true");
+        return xwikiParameters;
+    }
+}
