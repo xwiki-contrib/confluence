a6e9c1c4c47582f9013a6f65b6574dc14d1a2a51
Teo Caras <45433221+trrenty@users.noreply.github.com>
2024-09-06 11:56:36 +0300

    CONFLUENCE-271: Create a converter for macros that need to be converted into a group
    * CONFLUENCE-271: Create a converter for macros that need to be converted into a group
    
    * CONFLUENCE-271: Create a converter for macros that need to be converted into a group
    
    * Added since tags.
    
    * CONFLUENCE-271: Create a converter for macros that need to be converted into a group
    
    * Add missing headers
    * Add missing version tags

---
+++ confluence-xml/src/main/java/org/xwiki/contrib/confluence/filter/internal/macros/TextParamToContentConverter.java
@@ -0,0 +1,43 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.contrib.confluence.filter.internal.macros;
+
+import java.util.Map;
+
+import javax.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+
+/**
+ * Converts a macro call into a group, using the value of the "text" parameter as its content.
+ *
+ * @version $Id$
+ * @since 9.51.1
+ */
+@Component(hints = { "cfm-footnote", "cfm-tooltip", "tooltip" })
+@Singleton
+public class TextParamToContentConverter extends MacroToContentConverter
+{
+    @Override
+    protected String toXWikiContent(String confluenceId, Map<String, String> parameters, String confluenceContent)
+    {
+        return parameters.getOrDefault("text", "");
+    }
+}
