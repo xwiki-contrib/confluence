19abb1a0a51a40a2f9932d0161570a8f327fb8fa
farcasut <farcasialexandrububu@gmail.com>
2024-10-04 11:52:25 +0300

    CONFLUENCE-285: Convert img macro to xwiki syntax

---
+++ confluence-xml/src/main/java/org/xwiki/contrib/confluence/filter/internal/macros/ImgMacroConverter.java
@@ -0,0 +1,60 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.contrib.confluence.filter.internal.macros;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.contrib.confluence.filter.internal.input.ConfluenceConverter;
+import org.xwiki.rendering.listener.Listener;
+import org.xwiki.rendering.listener.reference.ResourceReference;
+
+/**
+ * Convert Confluence img macro to XWiki syntax.
+ *
+ * @version $Id$
+ * @since 9.55.2
+ */
+@Singleton
+@Component
+@Named("img")
+public class ImgMacroConverter extends AbstractMacroConverter
+{
+    private static final String SRC = "src";
+
+    @Inject
+    private ConfluenceConverter converter;
+
+    @Override
+    public void toXWiki(String confluenceId, Map<String, String> confluenceParameters, String confluenceContent,
+        boolean inline, Listener listener)
+    {
+        String url = confluenceParameters.get(SRC);
+        ResourceReference reference = converter.convertURL(url);
+        Map<String, String> parameters = new HashMap<>(confluenceParameters);
+        parameters.remove(SRC);
+        listener.onImage(reference, false, parameters);
+    }
+}
+
