590be7f5eed31d9be80cf52687ad0f2819cea256
Teodor Caras <trrenty@gmail.com>
2024-10-10 10:40:02 +0300

    CONFLUENCE-290: Link tags not handled properly when inside a macro parameter
    * Added missing java docs and license headers.

--- confluence-syntax-xhtml/src/main/java/org/xwiki/contrib/confluence/parser/xhtml/internal/wikimodel/ReferenceTagHandler.java
+++ confluence-syntax-xhtml/src/main/java/org/xwiki/contrib/confluence/parser/xhtml/internal/wikimodel/ReferenceTagHandler.java
@@ -1,3 +1,22 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.xwiki.contrib.confluence.parser.xhtml.internal.wikimodel;
 
 import org.xwiki.rendering.internal.parser.xhtml.wikimodel.XWikiReferenceTagHandler;
@@ -17,11 +36,17 @@ import static org.xwiki.contrib.confluence.parser.xhtml.internal.wikimodel.Confl
  *     <a href="https://www.bing.com/images/search?q=icon">https://www.bing.com/images/search?q=icon</a>
  *   </ac:parameter>
  * </ac:structured-macro>
+ *
+ * @version $Id$
+ * @since 9.57.0
  */
 public class ReferenceTagHandler extends TagHandler
 {
     private final XWikiReferenceTagHandler tagHandler;
 
+    /**
+     * @param tagHandler the default XWikiReference tag handler.
+     */
     public ReferenceTagHandler(XWikiReferenceTagHandler tagHandler)
     {
         super(tagHandler.isContentContainer());
