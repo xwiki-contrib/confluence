a61914bc06aaa05574908d793099f9803d5c94f4
Josué Tille <josue.tille@xwiki.com>
2024-10-14 16:07:50 +0200

    CONFLUENCE-293: Add support to convert widget macro to embed macro

---
+++ confluence-xml/src/main/java/org/xwiki/contrib/confluence/filter/internal/macros/WidgetMacroConverter.java
@@ -0,0 +1,59 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.contrib.confluence.filter.internal.macros;
+
+import java.util.Map;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.xwiki.component.annotation.Component;
+
+/**
+ * Convert Confluence widget macro to embed macro.
+ *
+ * @version $Id$
+ */
+@Component
+@Singleton
+@Named("widget")
+public class WidgetMacroConverter extends AbstractMacroConverter
+{
+    @Override
+    public String toXWikiId(String confluenceId, Map<String, String> confluenceParameters, String confluenceContent,
+        boolean inline)
+    {
+        return "embed";
+    }
+
+    @Override
+    protected String toXWikiParameterValue(String confluenceParameterName, String confluenceParameterValue,
+        String confluenceId, Map<String, String> parameters, String confluenceContent)
+    {
+        if (confluenceParameterValue != null
+            && ("width".equals(confluenceParameterName) || "height".equals(confluenceParameterName)))
+        {
+            // Remove everything which is not a number
+            // By example: 100px -> 100 OR 200 -> 200
+            return confluenceParameterValue.replaceAll(".*?(\\d+).*", "$1");
+        }
+        return confluenceParameterValue;
+    }
+}
