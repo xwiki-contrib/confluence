10b33b0ee95c666b2387709b245b146d7ee7f9e6
Raphaël Jakse <raphael.jakse@xwiki.com>
2024-10-31 09:29:34 +0100

    CONFLUENCE-311: Support the Markdown macro

---
+++ confluence-xml/src/main/java/org/xwiki/contrib/confluence/filter/internal/macros/MarkdownMacroConverter.java
@@ -0,0 +1,62 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.contrib.confluence.filter.internal.macros;
+
+import org.apache.commons.lang3.StringUtils;
+import org.xwiki.component.annotation.Component;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.util.Map;
+
+/**
+ * The markdown macro.
+ * @since 9.61.0
+ * @version $Id$
+ */
+@Named("markdown")
+@Component
+@Singleton
+public class MarkdownMacroConverter extends AbstractMacroConverter
+{
+    private static final String[] BRACKETS = {"{{", "[["};
+    private static final String[] ESCAPED_BRACKETS = {"{&#123;", "[&#91;"};
+
+    @Override
+    public String toXWikiId(String confluenceId, Map<String, String> confluenceParameters, String confluenceContent,
+        boolean inline)
+    {
+        return "content";
+    }
+
+    @Override
+    protected Map<String, String> toXWikiParameters(String confluenceId, Map<String, String> confluenceParameters,
+        String content)
+    {
+        return Map.of("syntax", "markdown/1.2");
+    }
+
+    @Override
+    protected String toXWikiContent(String confluenceId, Map<String, String> parameters, String confluenceContent)
+    {
+        // Escape {{ and [[ since they have no meaning in Confluence's markdown but they do have a meaning in our flavor
+        return StringUtils.replaceEach(confluenceContent, BRACKETS, ESCAPED_BRACKETS);
+    }
+}
