024a2c35630d4e483f98518af11d60368c6a6990
Josué Tille <josue.tille@xwiki.com>
2024-12-12 15:45:29 +0100

    CONFLUENCE-344: Add URL mapper for Scroll viewport hierarchical URL

--- confluence-url-mapping-scroll-viewport/src/test/java/org/xwiki/contrib/confluence/urlmapping/scrollviewport/internal/ConfluenceScrollViewportURLMapperTest.java
+++ confluence-url-mapping-scroll-viewport/src/test/java/org/xwiki/contrib/confluence/urlmapping/scrollviewport/internal/ConfluenceScrollViewportURLMapperTest.java
@@ -19,15 +19,24 @@
  */
 package org.xwiki.contrib.confluence.urlmapping.scrollviewport.internal;
 
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.xwiki.contrib.confluence.resolvers.ConfluencePageIdResolver;
-import org.xwiki.contrib.confluence.resolvers.ConfluenceResolverException;
+import org.xwiki.contrib.confluence.resolvers.ConfluenceScrollViewportSpacePrefixResolver;
 import org.xwiki.contrib.confluence.urlmapping.internal.ConfluenceURLMappingPrefixHandler;
 import org.xwiki.contrib.urlmapping.URLMappingResult;
 import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.DocumentReferenceResolver;
+import org.xwiki.model.reference.WikiReference;
+import org.xwiki.query.Query;
+import org.xwiki.query.QueryManager;
+import org.xwiki.query.QueryParameter;
 import org.xwiki.resource.entity.EntityResourceAction;
 import org.xwiki.resource.entity.EntityResourceReference;
 import org.xwiki.test.annotation.BeforeComponent;
@@ -35,14 +44,19 @@ import org.xwiki.test.annotation.ComponentList;
 import org.xwiki.test.junit5.mockito.ComponentTest;
 import org.xwiki.test.junit5.mockito.InjectMockComponents;
 import org.xwiki.test.junit5.mockito.MockComponent;
+import org.xwiki.wiki.descriptor.WikiDescriptorManager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.xwiki.contrib.confluence.urlmapping.internal.UrlMappingTestTools.assertFailedConversion;
 
 @ComponentTest
 @ComponentList({
-    ConfluenceScrollViewportFlatURLMapper.class
+    ConfluenceScrollViewportFlatURLMapper.class,
+    ConfluenceScrollViewportHierarchicalURLMapper.class,
 })
 public class ConfluenceScrollViewportURLMapperTest
 {
@@ -71,10 +85,42 @@ public class ConfluenceScrollViewportURLMapperTest
     @MockComponent
     private ConfluencePageIdResolver confluencePageIdResolver;
 
+    @MockComponent
+    private QueryManager queryManager;
+
+    @MockComponent
+    private Query mockQuery;
+
+    @MockComponent
+    private ConfluenceScrollViewportSpacePrefixResolver spacePrefixResolver;
+
+    @MockComponent
+    private DocumentReferenceResolver<String> resolver;
+
+    @MockComponent
+    private WikiDescriptorManager wikiDescriptorManager;
+
     @BeforeComponent
-    void setup() throws ConfluenceResolverException
+    void setup() throws Exception
     {
+        QueryParameter queryParameter = mock(QueryParameter.class);
         when(confluencePageIdResolver.getDocumentById(42)).thenReturn(MY_DOC_REF);
+        when(queryManager.createQuery(anyString(), any())).thenReturn(mockQuery);
+        when(mockQuery.<String>execute()).thenReturn(Collections.emptyList());
+        when(mockQuery.setWiki(anyString())).thenReturn(mockQuery);
+        when(mockQuery.bindValue(anyString(), anyString())).thenReturn(mockQuery);
+        when(mockQuery.bindValue(anyString())).thenReturn(queryParameter);
+        when(queryParameter.anyChars()).thenReturn(queryParameter);
+        when(queryParameter.query()).thenReturn(mockQuery);
+        when(queryParameter.literal(anyString())).thenReturn(queryParameter);
+        when(spacePrefixResolver.getSpaceAndPrefixForUrl("prefix/a/myspace/mydoc/webhome")).thenReturn(
+            new AbstractMap.SimpleImmutableEntry<>("prefix/a", "mySpace"));
+        when(wikiDescriptorManager.getAllIds()).thenReturn(Arrays.asList("xwiki", "sub"));
+        when(resolver.resolve(anyString(), any())).thenAnswer(i -> {
+            List<String> spaceList = new ArrayList<String>(Arrays.asList(((String) i.getArgument(0)).split("\\.")));
+            String pageName = spaceList.remove(spaceList.size() - 1);
+            return new DocumentReference(((WikiReference) i.getArgument(1)).getName(), spaceList, pageName);
+        });
     }
 
     @ParameterizedTest
@@ -82,8 +128,21 @@ public class ConfluenceScrollViewportURLMapperTest
         "prefix/a/1/page-ho-42.html?param=thatwedontcareabout",
         "prefix/a/1/page-ho-42.html"
     })
-    void convertViewPageActionURL(String path)
+    void convertViewportFlatURL(String path)
     {
+        URLMappingResult converted = handler.convert(path, GET, null);
+        assertEquals(DOC_RR, converted.getResourceReference());
+        assertEquals("", converted.getURL());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "prefix/a/myspace/mydoc/webhome?param=thatwedontcareabout",
+        "prefix/a/myspace/mydoc/webhome"
+    })
+    void convertViewportHierarchicalURL(String path) throws Exception
+    {
+        when(mockQuery.<String>execute()).thenReturn(Collections.singletonList("MigrationRoot.MySpace.MyDoc.WebHome"));
         URLMappingResult converted = handler.convert(path, GET, null);
         assertEquals(DOC_RR, converted.getResourceReference());
         assertEquals("", converted.getURL());
