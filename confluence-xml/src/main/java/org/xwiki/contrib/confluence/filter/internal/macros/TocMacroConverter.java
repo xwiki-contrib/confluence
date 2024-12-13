97d8b01e85873d2870e8a743d83d8a8fc42388ad
Raphaël Jakse <raphael.jakse@xwiki.com>
2024-12-13 11:18:19 +0100

    CONFLUENCE-345: Convert the toc macro, taking in account its minLevel, maxLevel and style=decimal parameters

---
+++ confluence-xml/src/main/java/org/xwiki/contrib/confluence/filter/internal/macros/TocMacroConverter.java
@@ -0,0 +1,67 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.contrib.confluence.filter.internal.macros;
+
+import org.apache.commons.lang3.StringUtils;
+import org.xwiki.component.annotation.Component;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Converts a macro call into a group, using the value of the "text" parameter as its content.
+ *
+ * @version $Id$
+ * @since 9.51.1
+ */
+@Component
+@Singleton
+@Named("toc")
+public class TocMacroConverter extends AbstractMacroConverter
+{
+    @Override
+    public String toXWikiId(String confluenceId, Map<String, String> confluenceParameters, String confluenceContent,
+        boolean inline)
+    {
+        return "toc";
+    }
+
+    @Override
+    protected Map<String, String> toXWikiParameters(String confluenceId, Map<String, String> confluenceParameters,
+        String confluenceContent)
+    {
+        Map<String, String> parameters = new HashMap<>();
+        String maxLevel = confluenceParameters.get("maxLevel");
+        if (StringUtils.isNotEmpty(maxLevel)) {
+            parameters.put("depth", maxLevel);
+        }
+        String minLevel = confluenceParameters.get("minLevel");
+        if (StringUtils.isNotEmpty(minLevel)) {
+            parameters.put("start", minLevel);
+        }
+
+        if ("decimal".equals(confluenceParameters.get("style"))) {
+            parameters.put("numbered", "true");
+        }
+        return parameters;
+    }
+}
