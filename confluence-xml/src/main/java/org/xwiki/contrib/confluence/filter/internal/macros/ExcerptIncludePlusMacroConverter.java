895ebc680099e622271d4d9c70aea065076d255b
Raphaël Jakse <raphael.jakse@xwiki.com>
2024-12-30 16:29:58 +0100

    CONFLUENCE-355: Add support for importing Scroll Versions excerpt-includeplus macro into XWiki
    * fix since version annotation

--- confluence-xml/src/main/java/org/xwiki/contrib/confluence/filter/internal/macros/ExcerptIncludePlusMacroConverter.java
+++ confluence-xml/src/main/java/org/xwiki/contrib/confluence/filter/internal/macros/ExcerptIncludePlusMacroConverter.java
@@ -38,7 +38,7 @@ import org.xwiki.model.reference.EntityReferenceSerializer;
  * Convert the excerpt-includeplus macro.
  *
  * @version $Id$
- * @since 9.68.0
+ * @since 9.69.0
  */
 @Singleton
 @Component
