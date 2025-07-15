94343164897fd7c1ee777deaadc27b7cf83c44f7
Raphaël Jakse <raphael.jakse@xwiki.com>
2025-07-15 10:41:34 +0200

    CONFLUENCE-485: Convert the popular-labels macro to tagcloud

---
+++ confluence-xml/src/main/java/org/xwiki/contrib/confluence/filter/internal/macros/PopularLabelsMacroConverter.java
@@ -0,0 +1,58 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.contrib.confluence.filter.internal.macros;
+
+import java.util.Map;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.apache.commons.lang3.StringUtils;
+import org.xwiki.component.annotation.Component;
+
+/**
+ * Convert the popular-labels macro to tagcloud.
+ * @since 9.88.0
+ * @version $Id$
+ */
+@Component
+@Singleton
+@Named("popular-labels")
+public class PopularLabelsMacroConverter extends AbstractMacroConverter
+{
+    @Override
+    public String toXWikiId(String confluenceId, Map<String, String> confluenceParameters, String confluenceContent,
+        boolean inline)
+    {
+        return "tagcloud";
+    }
+
+    @Override
+    protected Map<String, String> toXWikiParameters(String confluenceId, Map<String, String> confluenceParameters,
+        String content)
+    {
+        String spaceKey = confluenceParameters.get("spaceKey");
+        if (StringUtils.isEmpty(spaceKey)) {
+            throw new RuntimeException("the spaceKey is missing or empty, we don't know how to convert this");
+        }
+
+        return Map.of("space", spaceKey);
+    }
+}
